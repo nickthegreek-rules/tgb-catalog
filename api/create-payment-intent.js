74e09bf54989593e9617438b6dc5853278657810
nickthegreek-rules <nickaridas@gmail.com>
2026-06-12 12:22:59 +1000

    Update create-payment-intent.js

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -3,6 +3,7 @@ import Stripe from 'stripe';
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Content-Type', 'application/json');
 
   if (req.method === 'OPTIONS') {
